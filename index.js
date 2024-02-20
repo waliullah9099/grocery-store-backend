639ccaa485de0adf9416fff195c1f33686cbde02
Waliullah <waliullah9099@gmail.com>
2024-02-21 02:02:56 +0600

    readme.md file create

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://food-distribution2.netlify.app",
     credentials: true,
   })
 );
